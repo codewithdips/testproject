189a3855ae80681486e3680884c7abe102427ab4
Dipak Patil <patildips@88@gmail.com>
2022-12-05 13:57:07 +0530

    callection

---
+++ src/callection/maptest1.java
@@ -0,0 +1,38 @@
+package callection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class maptest1 {
+    public static void main(String[] args) {
+//        Map<Integer,String> students=new TreeMap<>();
+        //keys must be unique
+        Map<Integer,String> students=new HashMap<>();
+
+        students.put(30,"aniket");
+        students.put(40,"dipak");
+        students.put(10,"priyanka");
+        students.put(20,"dhanashree");
+        students.put(20,"dhanashree");
+        students.put(25,"dhanashree");
+
+        System.out.println(students);
+
+        Set<Map.Entry<Integer, String>> set=students.entrySet();
+        for(Map.Entry<Integer, String> value:set)
+        {
+            System.out.println(value.getKey()+10+" :   "+value.getValue());
+        }
+        if(students.containsKey(25))
+            System.out.println("present");
+        else
+            System.out.println("absent");
+
+        if(students.containsKey(125))
+            System.out.println("present");
+        else
+            System.out.println("absent");
+
+    }
+}
